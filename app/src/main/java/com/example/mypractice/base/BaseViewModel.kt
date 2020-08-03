334a69b161931c9ef02cbd7d42ee79a8d2ae1978
KirtiKashyap <kashyap.kirti11@gmail.com>
2020-08-03 22:32:37 +0100

    update

--- app/src/main/java/com/example/mypractice/base/BaseViewModel.kt
+++ app/src/main/java/com/example/mypractice/base/BaseViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.mypractice.base
 
-import android.app.Application
 import androidx.lifecycle.ViewModel
 import com.example.mypractice.injection.component.DaggerViewModelInjector
 import com.example.mypractice.injection.component.ViewModelInjector
@@ -9,14 +8,6 @@ import com.example.mypractice.viewmodel.UserListViewModel
 
 abstract class BaseViewModel : ViewModel() {
 
-    private var mApplication: Application? = null
-
-    /**
-     * Return the application.
-     */
-    open fun <T : Application?> getApplication(): T {
-        return mApplication as T
-    }
     private val injector: ViewModelInjector = DaggerViewModelInjector
         .builder()
         .networkModule(NetworkModule)
